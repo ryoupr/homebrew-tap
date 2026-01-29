868e7f9d9bf5bcee7f2510eb017a8b5b1b861544
Ryou6152 <55190661+ryoupr@users.noreply.github.com>
2026-01-29 10:18:19 +0900

    fix: add sha256 checksums

--- Formula/nimbus.rb
+++ Formula/nimbus.rb
@@ -10,11 +10,11 @@ class Nimbus < Formula
   on_macos do
     on_intel do
       url "https://github.com/ryoupr/nimbus/releases/download/v#{version}/nimbus-darwin-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+      sha256 "1881878feb0ba33a6b1d1942722f9752a83f1359244b78dae245f636a37e7e55"
     end
     on_arm do
       url "https://github.com/ryoupr/nimbus/releases/download/v#{version}/nimbus-darwin-arm64.tar.gz"
-      sha256 "REPLACE_WITH_ACTUAL_SHA256"
+      sha256 "41838ffbdcfbc8d14a527dcd087a468fd8dadb49e0b8c4a94806bf8bcd9c208c"
     end
   end
 
